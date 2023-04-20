5f5c94262abb270a3fadb258569256348faf32ec
Bohyeong.Lee <zqhgud@groupidd.com>
2023-04-20 11:57:01 +0900

    메인 작업중

--- __resource/js/app.js
+++ __resource/js/app.js
@@ -3,6 +3,7 @@ import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
 
 import {globalNav} from './common';
+import {main} from './main';
 import {benefitsLocal} from './benefits';
 import {trendList} from './trend';
 
@@ -11,6 +12,7 @@ const app = new App();
 app.add({globalNav});
 app.add({benefitsLocal});
 app.add({trendList});
+app.add({main});
 
 app.auto('globalNav');
 
--- __resource/js/main.js
+++ __resource/js/main.js
@@ -1,7 +1,18 @@
 import {find,findOne} from "./helper";
+import Swiper, {Autoplay, EffectFade, Pagination, Navigation} from "swiper";
 
 const main = () => {
 
+
+    //여행가는 달 참여기관
+    (() => {
+        const organization = findOne('.main__organization');
+        console.log(organization);
+
+        const carousel = new Swiper(findOne('.swiper', organization),{
+
+        });
+    })();
 
 };
 
