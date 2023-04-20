7cd126f6cae017dfc075426d062d72764acebb90
Bohyeong.Lee <zqhgud@groupidd.com>
2023-04-20 17:19:06 +0900

    메인 작업

--- __resource/js/main.js
+++ __resource/js/main.js
@@ -4,13 +4,21 @@ import Swiper, {Autoplay, EffectFade, Pagination, Navigation} from "swiper";
 const main = () => {
 
 
+
+    //이벤트
+    (() => {
+        const event = findOne('.main__event');
+        const eventCarousel = new Swiper(findOne('.swiper', event),{
+            slidesPerView: "auto",
+            spaceBetween: 20,
+        });
+    })();
+
     //여행가는 달 참여기관
     (() => {
         const organization = findOne('.main__organization');
-
-
-        const carousel = new Swiper(findOne('.swiper', organization),{
-            slidesPerView: 6,
+        const organizationCarousel = new Swiper(findOne('.swiper', organization),{
+            slidesPerView: "auto",
             spaceBetween: 100,
         });
     })();
