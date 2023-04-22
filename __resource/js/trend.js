0cdc66ca1224b2fc836958b90ca4962ce983309c
Byeonghwa.Yeon <hwalybell@groupidd.com>
2023-04-22 22:31:43 +0900

    레이아웃 수정

--- __resource/js/trend.js
+++ __resource/js/trend.js
@@ -1,41 +1,57 @@
 import {findOne, find, on} from './helper';
+import Swiper, {FreeMode} from 'swiper';
 
 const trendList = () => {
-    const modals = find('.modal-trend-content');
+    (() => {
+        const carouselContainer = findOne('.trend-list nav .swiper');
+        console.log(carouselContainer)
+        const carousel = new Swiper(carouselContainer, {
+            // slidesPerView: 3,
+            slidesPerView: 'auto',
+            freeMode: {
+                enabled: true,
+            },
+            modules: [FreeMode],
+        });
+    })();
 
-    modals.forEach(modal => (() => {
-        const tab = findOne('.modal-trend-content__tab', modal);
+    (() => {
+        const modals = find('.modal-trend-content');
 
-        if (!tab) return;
+        modals.forEach(modal => (() => {
+            const tab = findOne('.modal-trend-content__tab', modal);
 
-        const tabMenus = find('a', tab);
-        const tabPanels = find('.modal-trend-content__panel', modal);
-        const toggleTabPanels = (currentIndex) => {
+            if (!tab) return;
+
+            const tabMenus = find('a', tab);
+            const tabPanels = find('.modal-trend-content__panel', modal);
+            const toggleTabPanels = (currentIndex) => {
+                tabMenus.forEach((menu, index) =>
+                    menu.classList[
+                        currentIndex === index
+                            ? 'add'
+                            : 'remove'
+                        ]('modal-trend-content__tab-item--active'));
+                tabPanels.forEach((panel, index) =>
+                    panel.classList[
+                        currentIndex === index
+                            ? 'add'
+                            : 'remove'
+                        ]('modal-trend-content__panel--active'));
+            };
+
             tabMenus.forEach((menu, index) =>
-                menu.classList[
-                    currentIndex === index
-                        ? 'add'
-                        : 'remove'
-                    ]('modal-trend-content__tab-item--active'));
-            tabPanels.forEach((panel, index) =>
-                panel.classList[
-                    currentIndex === index
-                        ? 'add'
-                        : 'remove'
-                    ]('modal-trend-content__panel--active'));
-        };
+                on(menu, 'click', (event) => {
+                    event.preventDefault();
+                    toggleTabPanels(index);
+                })
+            );
 
-        tabMenus.forEach((menu, index) =>
-            on(menu, 'click', (event) => {
-                event.preventDefault();
-                toggleTabPanels(index);
-            })
-        );
-
-        on(modal, 'show.bs.modal', () =>
-            toggleTabPanels(0)
-        );
-    })());
+            on(modal, 'show.bs.modal', () =>
+                toggleTabPanels(0)
+            );
+        })());
+    })();
 };
 
 export {
