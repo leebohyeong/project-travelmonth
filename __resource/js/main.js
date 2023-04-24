8f4cabea81c7c83ebf82a070a3183d6afc9169da
Bohyeong.Lee <zqhgud@groupidd.com>
2023-04-24 14:52:55 +0900

    images 경로변경 및 메인 작업중

--- __resource/js/main.js
+++ __resource/js/main.js
@@ -1,11 +1,43 @@
-import {find,findOne} from "./helper";
+import {find, findOne, on} from "./helper";
 
 import Swiper, {Autoplay, EffectFade, Pagination, Navigation, Scrollbar} from "swiper";
 
 const main = () => {
 
     //tab
+    (() => {
+        const category = findOne('.main__category');
+        const tabMenus = find('.category-tab a', category);
+        const tabPanels = find(`.category-panel`, category);
+        const loaction = findOne('.category-tab-contents__location', category);
+        const program = findOne('.category-tab-contents__program', category);
 
+        const toggleTabPanels = (currentIndex) => {
+            tabMenus.forEach((menu, index) =>
+                menu.classList[
+                    currentIndex === index
+                        ? 'add'
+                        : 'remove'
+                    ](`category-tab__item--active`));
+            tabPanels.forEach((panel, index) =>
+                panel.classList[
+                    currentIndex === index
+                        ? 'add'
+                        : 'remove'
+                    ](`category-panel--active`));
+        };
+
+        tabMenus.forEach((menu, index) =>
+            on(menu, 'click', (event) => {
+                event.preventDefault();
+
+                toggleTabPanels(index);
+            })
+        );
+
+        loaction
+
+    })();
 
     //트렌드
     (() => {
