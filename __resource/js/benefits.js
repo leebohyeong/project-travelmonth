e430219b5ad5f90444f87605a985d0b3e62d790e
Byeonghwa.Yeon <hwalybell@groupidd.com>
2023-04-27 22:17:37 +0900

    놀거리 수정

--- __resource/js/benefits.js
+++ __resource/js/benefits.js
@@ -2,17 +2,29 @@ import {findOne, find, on} from './helper';
 
 const benefitsLocal = () => {
     const menu = findOne('.benefits__menu--local nav + div');
-    const currentItem = findOne('strong', menu).closest('li');
-    const localItems = find('a', menu);
-    const activeLocalClassName = 'benefits__menu-item--local-hide';
-    const showLocal = () => currentItem.classList.remove(activeLocalClassName);
-    const removeLocal = () => currentItem.classList.add(activeLocalClassName);
-    const togglEvent = (item) => {
-        on(item, 'mouseenter', removeLocal);
-        on(item, 'mouseleave', showLocal);
-    };
 
-    // localItems.forEach(item => togglEvent(item));
+    (() => {
+        const currentItem = findOne('strong', menu).closest('li');
+        const localItems = find('a', menu);
+        const activeLocalClassName = 'benefits__menu-item--local-hide';
+        const showLocal = () => currentItem.classList.remove(activeLocalClassName);
+        const removeLocal = () => currentItem.classList.add(activeLocalClassName);
+        const togglEvent = (item) => {
+            on(item, 'mouseenter', removeLocal);
+            on(item, 'mouseleave', showLocal);
+        };
+
+        // localItems.forEach(item => togglEvent(item));
+    })();
+
+    (() => {
+        const select = findOne('select', menu);
+        const options = find('option', select);
+
+        on(select, 'change', () => {
+            location.href = options[select.selectedIndex].dataset.url;
+        });
+    })();
 };
 
 export {
